5b627f23492aded99c8a1d81d2e11bfa72ae4785
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2021-04-30 00:24:38 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod text_plotter;
+
 use std::net::SocketAddr;
 use structopt::StructOpt;
+use text_plotter::{Range, TextPlotter};
 use tokio::net::TcpListener;
 use tokio_stream::StreamExt;
 use tokio_util::codec::{Framed, LinesCodec};
@@ -42,79 +45,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         });
-    }
-}
-
-fn calculate_bar_width(x: f64, min: f64, max: f64, bar_capacity: usize) -> usize {
-    let y = ((x - min) / (max - min)) * (bar_capacity as f64);
-    if y < 0.0 {
-        0
-    } else {
-        y as usize
-    }
-}
-
-fn bar_to_string(bar_width: usize) -> String {
-    std::iter::repeat("-").take(bar_width).collect::<String>()
-}
-
-fn print_bar(bar_width: usize) {
-    println!("{}", bar_to_string(bar_width));
-}
-
-#[derive(Clone)]
-struct TextPlotter {
-    bar_capacity: usize,
-    range: Range,
-}
-
-impl TextPlotter {
-    fn new(bar_capacity: usize, range: Range) -> Self {
-        TextPlotter {
-            bar_capacity,
-            range,
-        }
-    }
-
-    fn update(&self, x: f64) {
-        let bar_width = calculate_bar_width(x, self.range.min, self.range.max, self.bar_capacity);
-        print_bar(bar_width);
-    }
-}
-
-#[derive(Clone)]
-struct Range {
-    min: f64,
-    max: f64,
-}
-
-impl Range {
-    fn new(min: f64, max: f64) -> Self {
-        Range { min, max }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn calculate_bar_width_test() {
-        assert_eq!(calculate_bar_width(10.0, 0.0, 100.0, 50), 5);
-        assert_eq!(calculate_bar_width(50.0, 30.0, 130.0, 100), 20);
-        assert_eq!(calculate_bar_width(10.0, 30.0, 100.0, 50), 0);
-    }
-
-    #[test]
-    fn bar_to_string_test() {
-        assert_eq!(bar_to_string(8), "--------");
-        assert_eq!(bar_to_string(10), "----------");
-    }
-
-    #[test]
-    fn terminal_plotter_test() {
-        let range = Range::new(0.0, 100.0);
-        let tp = TextPlotter::new(100, range);
-        tp.update(50.0);
     }
 }
---
+++ src/text_plotter.rs
@@ -0,0 +1,73 @@
+fn calculate_bar_width(x: f64, min: f64, max: f64, bar_capacity: usize) -> usize {
+    let y = ((x - min) / (max - min)) * (bar_capacity as f64);
+    if y < 0.0 {
+        0
+    } else {
+        y as usize
+    }
+}
+
+fn bar_to_string(bar_width: usize) -> String {
+    std::iter::repeat("-").take(bar_width).collect::<String>()
+}
+
+fn print_bar(bar_width: usize) {
+    println!("{}", bar_to_string(bar_width));
+}
+
+#[derive(Clone)]
+pub struct TextPlotter {
+    bar_capacity: usize,
+    range: Range,
+}
+
+impl TextPlotter {
+    pub fn new(bar_capacity: usize, range: Range) -> Self {
+        TextPlotter {
+            bar_capacity,
+            range,
+        }
+    }
+
+    pub fn update(&self, x: f64) {
+        let bar_width = calculate_bar_width(x, self.range.min, self.range.max, self.bar_capacity);
+        print_bar(bar_width);
+    }
+}
+
+#[derive(Clone)]
+pub struct Range {
+    min: f64,
+    max: f64,
+}
+
+impl Range {
+    pub fn new(min: f64, max: f64) -> Self {
+        Range { min, max }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calculate_bar_width_test() {
+        assert_eq!(calculate_bar_width(10.0, 0.0, 100.0, 50), 5);
+        assert_eq!(calculate_bar_width(50.0, 30.0, 130.0, 100), 20);
+        assert_eq!(calculate_bar_width(10.0, 30.0, 100.0, 50), 0);
+    }
+
+    #[test]
+    fn bar_to_string_test() {
+        assert_eq!(bar_to_string(8), "--------");
+        assert_eq!(bar_to_string(10), "----------");
+    }
+
+    #[test]
+    fn terminal_plotter_test() {
+        let range = Range::new(0.0, 100.0);
+        let tp = TextPlotter::new(100, range);
+        tp.update(50.0);
+    }
+}
