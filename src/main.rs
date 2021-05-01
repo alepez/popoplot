a2161cf78b1f0afb3bf1a4721e61beb2b6487eb4
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2021-05-02 01:00:26 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -35,14 +35,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(opt.bind).await?;
 
     let range = Range::new(opt.min, opt.max);
-    let output = std::io::stdout();
 
     let mult_conn = opt.multiple_connections;
 
     if mult_conn {
-        launch_multiple_connections_server(opt, listener, range, output).await
+        launch_multiple_connections_server(opt, listener, range).await
     } else {
-        launch_single_connection_server(opt, listener, range, output).await
+        launch_single_connection_server(opt, listener, range).await
     }
 }
 
@@ -50,9 +49,8 @@ async fn launch_multiple_connections_server(
     opt: Opt,
     listener: TcpListener,
     range: Range,
-    output: std::io::Stdout,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let tp = TextPlotter::new(opt.bar_capacity, range, output);
+    let tp = TextPlotter::new(opt.bar_capacity, range, std::io::stdout());
     let tp = Arc::new(Mutex::new(tp));
 
     loop {
@@ -71,9 +69,8 @@ async fn launch_single_connection_server(
     opt: Opt,
     listener: TcpListener,
     range: Range,
-    output: std::io::Stdout,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let tp = TextPlotter::new(opt.bar_capacity, range, output);
+    let tp = TextPlotter::new(opt.bar_capacity, range, std::io::stdout());
     let tp = Arc::new(Mutex::new(tp));
 
     loop {
